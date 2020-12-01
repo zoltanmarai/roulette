719961181cb47f79250b7facc854e4cc9ae769de
zoltanmarai <zoltanmarai51@gmail.com>
2020-12-01 10:51:55 +0100

    Jól működő alap verzió

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,13 @@ public class Main {
         roulette.setPrizeList();
         MartingelBela mb = new MartingelBela();
         RandomRudolf rr = new RandomRudolf();
+        GyavaGyuszi gg = new GyavaGyuszi();
+        BatorBotond bb = new BatorBotond();
         luckyPack.add(mb);
         luckyPack.add(rr);
+        luckyPack.add(gg);
+        luckyPack.add(bb);
+
         int menu;
         Scanner si = new Scanner(System.in);
 do {
@@ -94,23 +99,23 @@ do {
         int pocketMoney = sc.nextInt();
         while ( play) {
             System.out.println("Mire tennél?");
-            System.out.println("Pirosra -----> 1-es gomb");
-            System.out.println("Feketére -----> 2-es gomb");
-            System.out.println("Párosra -----> 3-es gomb");
-            System.out.println("Páratlanra -----> 4-es gomb");
-            System.out.println("1.fél(1-18)-----> 5-es gomb");
-            System.out.println("2.fél(19-36) -----> 6-es gomb");
-            System.out.println("1.harmad(1-12) -----> 7-es gomb");
+            System.out.println("Pirosra -------------> 1-es gomb");
+            System.out.println("Feketére ------------> 2-es gomb");
+            System.out.println("Párosra -------------> 3-es gomb");
+            System.out.println("Páratlanra ----------> 4-es gomb");
+            System.out.println("1.fél(1-18) ---------> 5-es gomb");
+            System.out.println("2.fél(19-36) --------> 6-es gomb");
+            System.out.println("1.harmad(1-12) ------> 7-es gomb");
             System.out.println("2.harmad(13-24) -----> 8-es gomb");
             System.out.println("3.harmad(25-36) -----> 9-es gomb");
-            System.out.println("1.oszlop -----> 10-es gomb");
-            System.out.println("2.oszlop -----> 11-es gomb");
-            System.out.println("3.oszlop -----> 12-es gomb");
-            System.out.println("1 db számra -----> 13-es gomb");
-            System.out.println("2 db számra -----> 14-es gomb");
-            System.out.println("3 db számra -----> 15-es gomb");
-            System.out.println("4 db számra -----> 16-es gomb");
-            System.out.println("6 db számra -----> 17-es gomb");
+            System.out.println("1.oszlop -----------> 10-es gomb");
+            System.out.println("2.oszlop -----------> 11-es gomb");
+            System.out.println("3.oszlop -----------> 12-es gomb");
+            System.out.println("1 db számra --------> 13-es gomb");
+            System.out.println("2 db számra --------> 14-es gomb");
+            System.out.println("3 db számra --------> 15-es gomb");
+            System.out.println("4 db számra --------> 16-es gomb");
+            System.out.println("6 db számra --------> 17-es gomb");
             int var = sc.nextInt();
             prizeClass = new PrizeClass(roulette.prizeList.get(var-1).getName(), roulette.prizeList.get(var-1).getMultiplier(), roulette.prizeList.get(var-1).getOpposite());
 
@@ -132,12 +137,13 @@ do {
                         bet = 0;
                     }
                     System.out.println(" Ebben a körben ennyi pénzt nyertél: " + bet);
-
-                } else {
+                    pocketMoney += bet;
+                }  else  if (pocketMoney+bet <= 0){
                     System.out.println("Nincs elég pénzed.");
+                } else {
+                    pocketMoney+=bet;
                 }
             }else{
-
                 List<Integer> numbers = new ArrayList<>();
                 int num;
                 int number;
@@ -151,14 +157,21 @@ do {
                         numbers.add(number);
                         break;
                     case 14:
+
                         do {
                             System.out.println("Add meg a számot");
                             number = sc.nextInt();
                         }while (!(number>=0 && number<=33));
+                        boolean a;
                         do {
+                             a = false;
                             System.out.println("Add meg a 2. számot");
                             num = sc.nextInt();
-                        }while ((num != number+1 && num != number+3));
+                            if(number % 3 == 0 && num == number+1){
+                                a = true;
+                            }
+
+                        }while ((num != number+1 && num != number+3) || a);
                         numbers.add(number);
                         numbers.add(num);
                         break;
@@ -175,7 +188,7 @@ do {
                         do {
                             System.out.println("Add meg a számot, a kombinációból, a legkisebbet.");
                             number = sc.nextInt();
-                        }while (!(number>=0 && number<=32) || (number % 3!=0));
+                        }while (!(number>=0 && number<=32) || (number % 3==0));
                         numbers.add(number);
                         numbers.add(number+1);
                         numbers.add(number+3);
@@ -208,16 +221,18 @@ do {
                         System.out.println(numbers.get(i));
                         if (win == numbers.get(i)) {
                             bet1= bet *prizeClass.getMultiplier();
+
                         }
                     }
                     System.out.println(" Ebben a körben ennyi pénzt nyertél: " + bet1);
-
-                } else {
+                    pocketMoney += bet1;
+                } else  if (pocketMoney+bet <= 0){
                     System.out.println("Nincs elég pénzed.");
+                } else {
+                    pocketMoney+=bet;
                 }
             }
 
-            pocketMoney += bet1;
             System.out.println(" Összesen ennyi pénzed van most: " + pocketMoney);
 
             System.out.println("Akarsz még játszani?(igen/nem)");
